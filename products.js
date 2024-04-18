5a085c75790cfacfe1f9dd23815e51ee8fa33092
Upadhyayme <143839565+Upadhyayme@users.noreply.github.com>
2024-04-18 12:25:53 +0530

    file added

---
+++ products.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+class company_display extends React.Component 
+{
+    render()
+    {
+        return (
+            <div classname="product">
+            {"AMZ phone"} 
+            <h1>{this.props.phone}</h1>
+        <p>{this.props.(20000)}</p>
+        {"this AMZ phone has a memory of 128 GB , has four camera. this been one of the best sale of the year"}
+        </div>
+        
+    <div classname="product1">
+    {"FLIP Earphone"} 
+    <h2>{this.props.Earphone}</h2>
+    <p1>{this.props.(40000)}</p1>
+   { this FLIP Earphone. this been one of the best sale of the year 2024 }
+   </div>
+
+        <div>
+
+            <nav class="company_sale">
+
+                <ul class="list">
+
+                    <div class="products">
+
+                        />
+
+                    </div>
+
+                    <li>
+
+                        <a href="#phone">phone</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="earphone">earphone</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="#computer">computer</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="#laptop">Laptop</a>
+
+                    </li>
+
+                </ul>
+ 
+
+                <div class="rightNav">
+
+                    <input type="text" name="search" />
+
+                    <button class="press">Search</button></div>
+
+            </nav>
+ 
+
+            <section class="product">
+
+                <div class="box-main">
+
+                    <div class="details">
+
+                        <h1 class="text-big">all products Search in 2024</h1>
+
+                        <p class="text-small"> search the best </p>
+
+                    </div>
+
+                </div>
+            <footer className="footer">
+
+                <p className="text-footer">all best products</p> </footer>
+ </div>
+
+);
+    }
+}
+
+
